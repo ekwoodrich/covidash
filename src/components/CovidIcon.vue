1fdce0d466169d0fe9d80f492cb8ccc244759cd0
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2020-07-20 21:32:47 -0700

    Added grid.

---
+++ src/components/CovidIcon.vue
@@ -0,0 +1,8 @@
+Vue.component('covid-icon', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
